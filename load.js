805034b4f886935311187e6e686714812706da46
Bruno Michel <bmichel@menfin.info>
2017-04-11 09:01:34 +0200

    Use HTTPS for OSM

--- load.js
+++ load.js
@@ -1,5 +1,5 @@
 var map = L.map('maposm').setView([51.505, -0.09], 13);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
